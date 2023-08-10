e152e3e0f51a12243727e36bcda0fc8c29aedcb8
leonard Akpofure <77726124+leonovis@users.noreply.github.com>
2023-08-10 09:42:48 +0100

    starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,17 +1,35 @@
 #!/usr/bin/node
-const request = require('request');
-const url = 'http://swapi.co/api/films/' + process.argv[2];
 
-request(url, function (err, response, body) {
-  if (err == null) {
-    const resp = JSON.parse(body);
-    const characters = resp.characters;
-    for (let i = 0; i < characters.length; i++) {
-      request(characters[i], function (err, response, body) {
-        if (err == null) {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    }
+const request = require('request');
+
+const movieId = process.argv[2];
+const url = `https://swapi.dev/api/films/${movieId}/`;
+
+request.get(url, { json: true }, (error, response, data) => {
+  if (error) {
+    console.error(error);
+    return;
   }
+
+  const charactersUrls = data.characters;
+
+  // Function to fetch character data and print their names
+  function fetchCharacterName (characterUrl) {
+    request.get(characterUrl, { json: true }, (error, response, characterData) => {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log(characterData.name);
+      }
+
+      // Fetch the next character's name until all characters are printed
+      const nextCharacterUrl = charactersUrls.shift();
+      if (nextCharacterUrl) {
+        fetchCharacterName(nextCharacterUrl);
+      }
+    });
+  }
+
+  // Start fetching character names
+  fetchCharacterName(charactersUrls.shift());
 });
